8c55f19a1b8f42a1bacc98b37a148548a2a029b9
chanduatnonceblox <chandra.shekhar@nonceblox.com>
2024-11-07 16:36:54 +0530

    first commit

--- src/price/price.controller.ts
+++ src/price/price.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { PriceService } from './price.service';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Price')
 @Controller('prices')
 export class PriceController {
   constructor(private readonly pricesService: PriceService) {}
 
   @Get('/:chain/hourly')
+  @ApiOperation({ summary: 'Get latest price by chain' })
+  @ApiParam({
+    name: 'chain',
+    description: 'Blockchain name (e.g., ethereum, polygon)',
+  })
   async getHourlyPrices(@Param('chain') chain: string) {
     return await this.pricesService.getHourlyPrices(chain);
   }
